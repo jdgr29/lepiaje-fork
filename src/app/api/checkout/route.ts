e16d8338e7336aa7e842e31311e6074173110112
J.Daniel <juandaniel9619@gmail.com>
2024-12-22 18:50:13 -0400

    fix: lint error

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 //TODO this route will handle stripe payments along with notifications via email
 export default async function POST(req: Request) {
+    console.log("req", req)
     return NextResponse.json({ data: "everything good" }, { status: 200 })
 }
 
--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,11 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request:Request) {
-<<<<<<< HEAD
     console.log("route working as expected", process.env.DB_CONNECTION_STRING);
-=======
     console.log("route working as expected", request);
->>>>>>> main
     return NextResponse.json({data: {
         text:"hello world", 
         error:false,
