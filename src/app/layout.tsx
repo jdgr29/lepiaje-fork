2e8052b42f1a35f81983958bcc630a3199c79578
J.Daniel <juandaniel9619@gmail.com>
2024-12-26 14:49:50 -0400

    feat:added whatsapp and mail icons to landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
+import WhatsAppAndEmail from "@/components/whatsappEmailButtons/whatsappEmailButtons";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <WhatsAppAndEmail />
       </body>
     </html>
   );
---
+++ src/components/whatsappEmailButtons/whatsappEmailButtons.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+import { Mail } from "lucide-react";
+import { FaWhatsapp } from "react-icons/fa6";
+
+export default function WhatsAppAndEmail() {
+  //TODO for email better smoothly scroll to the form since is the same thing and mailto: is buggy af
+
+  const phoneNumber = ""; //number to whatsapp  here, no symbols just number. no empty spaces //TODO should come from .env
+  const message = "Hello, I liked the Le Piaje website!"; // Pre-filled message but really this is optional
+  const encodedMessage = encodeURIComponent(message);
+
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+  return (
+    <div className="z-50 fixed bottom-0 right-0 pr-4 pb-4">
+      <div className="flex items-center gap-x-4 justify-center">
+        <div
+          style={{ borderRadius: "100%" }}
+          className="flex p-1 items-center justify-center h-[2em] w-[2em] bg-[#ffff] transition-all ease-linear hover:scale-110 active:scale-90"
+        >
+          <Mail color="#121212" size={20} />
+        </div>
+        <div
+          style={{ borderRadius: "100%" }}
+          className="flex p-1 items-center justify-center h-[2em] w-[2em] bg-[#ffff] transition-all ease-linear hover:scale-110 active:scale-90"
+        >
+          <Link target="_blank" href={whatsappUrl}>
+            <FaWhatsapp color="#25d366" size={21} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
