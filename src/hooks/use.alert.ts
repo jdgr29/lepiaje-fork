7ea9d12f7c9c366c215b7028dba21fde177abe37
J.Daniel <juandaniel9619@gmail.com>
2024-12-31 10:50:09 -0400

    chore:prepare for merge with main

---
+++ src/hooks/use.alert.ts
@@ -0,0 +1,19 @@
+'use client'
+import { useState } from 'react'
+
+export function useSuccessAlert() {
+    const [isVisible, setIsVisible] = useState(false)
+    const [message, setMessage] = useState('')
+
+    const showAlert = (newMessage: string) => {
+        setMessage(newMessage)
+        setIsVisible(true)
+    }
+
+    const hideAlert = () => {
+        setIsVisible(false)
+    }
+
+    return { isVisible, message, showAlert, hideAlert }
+}
+
